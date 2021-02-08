8b6db8cf518576f319bb0f19081f3a55853543e7
Tatiana655 <61006466+Tatiana655@users.noreply.github.com>
2021-02-08 11:44:24 +0300

    Add a little changes
    Renames and some little mistakes

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 import numpy as np
 
 
-def find_first_vec(A, b, N):
-    A = np.array(A).transpose()
-    for i in range(N):
-        for j in range(i, N):
-            for k in range(j, N):  # составить матрицу из столбцов, решить методом наусса проверить на положительность
-                A_help = np.array([A[i], A[j], A[k]]).transpose()
-                if np.linalg.det(A_help) != 0:
-                    first_vec = np.linalg.solve(A_help, b)
-                    if (first_vec[0] != first_vec[1]) & (first_vec[0] != first_vec[2]) & (
-                            first_vec[2] != first_vec[1]) & (np.ma.amin(first_vec) >= 0):
-                        # заполнить нулями
-                        vec = np.array([0., 0., 0., 0., 0., 0.])
-                        vec[i] = first_vec[0]
-                        vec[j] = first_vec[1]
-                        vec[k] = first_vec[2]
-                        return vec
-
-
 def find_j_k(N_k_0, d_k):
     for i in range(len(N_k_0)):
         if (d_k[N_k_0[i]] < 0):
@@ -28,60 +10,56 @@ def find_j_k(N_k_0, d_k):
 
 # начальные данные
 # MxN #3x5
-M = 3
-N = 5
-c = [-7, -8.2, -8.6, 0, 0, 0]
+c = [-8.2, -9, -9.6, 1, -1, -1, 0, 0, 0]  # [-7, -8.2, -8.6, 0, 0, 0]
 
 A = [
-    [0.6, 0.3, 0.5, -1, 0, 0],
-    [0.1, 0.2, 0.1, 0, 1, 0],
-    [0.5, 0.3, 0.4, 0, 0, 1]
+    [0.6, 0.3, 0.5, -1, 0, 0, 1, 0, 0],
+    [0.1, 0.2, 0.1, 0, 1, 0, 0, 1, 0],
+    [0.5, 0.3, 0.4, 0, 0, 1, 0, 0, 1]
 ]
 
-b = np.array([120, 30, 100]).transpose()
+b = [120, 30, 100]
 
-# транспонировали, чтобы добыть столбцы
 # найти первый опорный вектор
-x = find_first_vec(A, b, N)
+x = np.array([0, 0, 0, 0, 0, 0, 120, 30, 100])
 while (1):
-    N_k_p = [index for index, data in enumerate(x) if x[index] != 0]
+    N_k_p = [index for index, data in enumerate(x) if x[index] >= 0]
     N_k_0 = [index for index, data in enumerate(x) if x[index] == 0]
+    # количество в _k_p == 3 иначе пополнить и определитель не 0
+    # ввести N1 и L1?
+
+    Nk = N_k_p
+    Lk = N_k_0
 
     A = np.array(A).transpose()
-    # if на случай если вдруг меньше, определитель не 0
+    A_M_Nk = np.transpose(np.matrix([np.array(A[Nk[0]]), np.array(A[Nk[1]]), np.array(A[Nk[2]])]))  # [Nk]#!!!!!
+    c_Nk = np.array([c[Nk[0]], c[Nk[1]], c[Nk[2]]])
 
-    A_help = np.matrix([np.array(A[N_k_p[0]]), np.array(A[N_k_p[1]]), np.array(A[N_k_p[2]])])  # [Nk]#!!!!!
+    Bk = np.linalg.inv(A_M_Nk)
+    yk = c_Nk * Bk
 
-    L_k = N_k_0  # N-N_k
-    c_help = np.array([c[N_k_p[0]], c[N_k_p[1]], c[N_k_p[2]]])  # [Nk]
-
-    B = np.linalg.inv(A_help)
-    y_k = np.transpose(c_help) * B
-
-    d_k = np.transpose(c) - np.array(y_k) * np.transpose(np.matrix(A))
-    d_k = np.array(d_k)[0]
-
-    d_k_help = np.array([d_k[N_k_0[0]], d_k[N_k_0[1]], d_k[N_k_0[2]]])
-
-    if (np.ma.amin(d_k_help) >= 0):
+    dk = np.transpose(c) - np.array(yk) * np.transpose(np.matrix(A))
+    d_non = np.array([])
+    d_non = np.append(d_non, dk[0])
+    d_Lk = [d_non[index] for index, data in enumerate(d_non) if d_non[index] != 0]
+    if np.ma.amin(d_Lk) >= 0:
+        sol = x
         break
-    j_k = find_j_k(N_k_0, d_k)
+    else:
+        j_k = find_j_k(Lk, d_Lk) #возвращает номер первого отрицательного
+        u_Nk = np.array([])
+        u_Nk =np.append(u_Nk, np.transpose(Bk * np.matrix(np.transpose([A[j_k]]))))
+        u_k = np.zeros(9)
+        u_k[j_k] = -1
+        for i in range(len(N_k_p)):
+            u_k[N_k_p[i]] = u_Nk[i]
+        #если кто-то из Nk <0 то умирай. Всё плохо. тут такого типа нет
 
-    # print(A[j_k])
-    # print(B)
-    u_k_help = (B * np.matrix(np.transpose([A[j_k]])))
+        theta_k = x[N_k_p[0]] / u_k[N_k_p[0]]
+        for i in range(len(N_k_p)):
+            if (u_k[N_k_p[i]] > 0):
+                theta_k = min(x[N_k_p[i]] / u_k[N_k_p[i]], theta_k)
 
-    u_k = [0, 0, 0, 0, 0, 0]
-    u_k[j_k] = -1
-    u_k[N_k_p[0]] = u_k_help[0, 0]
-    u_k[N_k_p[1]] = u_k_help[1, 0]
-    u_k[N_k_p[2]] = u_k_help[2, 0]
-    theta_k = x[N_k_p[0]] / u_k[N_k_p[0]]
-    for i in range(len(N_k_p)):
-        if (u_k[N_k_p[i]] > 0):
-            theta_k = min(x[N_k_p[i]] / u_k[N_k_p[i]], theta_k)
-    x_theta = x - theta_k * np.array(u_k)
-    print(x_theta)
-    print("F = ", np.array(c) * np.transpose(x_theta))
-
-print(c*x_theta)
+        x = x - theta_k * np.array(u_k)
+        print(x)
+print(x)
