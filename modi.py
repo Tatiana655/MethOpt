4189ad40ac2467b85ac67955448aec7d669d2709
Tatiana655 <61006466+Tatiana655@users.noreply.github.com>
2021-02-22 19:52:44 +0300

    Update modi.py

--- modi.py
+++ modi.py
@@ -1,40 +1,45 @@
 import numpy as np
 
+
 # типа найти максимальную разность и вернуть индексы (не запутаться бы в них). тут тоже питоновские штуки
-def rightPoint(C, u,v):
+def rightPoint(C, u, v):
     max1 = -1
     max_i = -1
     max_j = -1
     for j in range(len(v)):
         for i in range(len(u)):
-            if v[j] - u[i] > C[i][j]:
-              if (max1 != max(max1, abs(v[j] - u[i] - C[i][j]))):
-                max_i = i#string u
-                max_j = j#col v
-                max1 = max(max1, abs((v[j] - u[i]) - C[i][j]))
+            if (v[j] - u[i]) > C[i][j]:
+                if (max1 != max(max1, abs((v[j] - u[i]) - C[i][j]))):
+                    max_i = i  # string u
+                    max_j = j  # col v
+                    max1 = max(max1, abs((v[j] - u[i]) - C[i][j]))
     return max_i, max_j
 
-array = [] #не  забыть добавить сюда первый элемент
 
-def Modi(bol,A, i , j, flag):
+array = []  # не  забыть добавить сюда первый элемент in code
+
+
+def Modi(bol, A, i, j, flag):
     elem = []
     if (len(array) == 2) and (array[0][0] == array[1][0] and array[0][1] == array[1][1] and flag > 0):
         return 0
     if i == array[0][0] and j == array[0][1] and flag > 0:
         return 1
 
-    if bol:#ищем элемент на вертикали
-        for i_k in range(len(A)):#or единственый нулевой элемент тот что в самом начале подошёл
-            if ((i_k != i) and (A[i_k][j] != 0) and (array.count([i_k, j]) == 0)) or ((i_k == array[0][0]) and (j == array[0][1])):
+    if bol:  # ищем элемент на вертикали
+        for i_k in range(len(A)):  # or единственый нулевой элемент тот что в самом начале подошёл
+            if ((i_k != i) and (A[i_k][j] != 0) and (array.count([i_k, j]) == 0)) or (
+                    (i_k == array[0][0]) and (j == array[0][1])):
                 elem.append([i_k, j])
-                #array.append([i_k, j])
-    else:#ищем элемент на горизонтали
+                # array.append([i_k, j])
+    else:  # ищем элемент на горизонтали
         for j_k in range(len(A[0])):
-            if ((j_k !=j) and (A[i][j_k] != 0) and (array.count([i, j_k]) == 0)) or ((i == array[0][0]) and (j_k == array[0][1])):
+            if ((j_k != j) and (A[i][j_k] != 0) and (array.count([i, j_k]) == 0)) or (
+                    (i == array[0][0]) and (j_k == array[0][1])):
                 elem.append([i, j_k])
-                #array.append([i, j_k])
-    #наверное тут надо получать все элеемнты и бегать циклом и вызывать моди
-    #и где-то надо чекнуть, что элементы не повторяются,в if'е, наверное
+                # array.append([i, j_k])
+    # наверное тут надо получать все элеемнты и бегать циклом и вызывать моди
+    # и где-то надо чекнуть, что элементы не повторяются,в if'е, наверное
     if not elem:
         return 0
     for e in elem:
@@ -44,7 +49,8 @@ def Modi(bol,A, i , j, flag):
         else:
             array.remove(e)
 
-def findMin(array, x0):#а может и вектор C
+
+def findMin(array, x0):
     mina = 10e5
     for k in range(0, len(array), 2):
         mina = min(mina, x0[array[k][0]][array[k][1]])
@@ -52,7 +58,7 @@ def findMin(array, x0):#а может и вектор C
 
 
 def Alg(x1):
-    while(1):
+    while (1):
         # A = np.zeros((len(C), len(C[0])))
         # for i in range(len(C)):
         #    for j in range(len(C[0])):
@@ -62,23 +68,31 @@ def Alg(x1):
 
         # число заполненных клеток n+m-1 (верно)
 
-        v = np.zeros(len(x1[0]))
-        u = np.zeros(len(x1))
-        v[0] = x1[0][0] #hardcode!!!
+        #        v = np.zeros(len(x1[0]))
+        #        u = np.zeros(len(x1))
+        # v[0] = x1[0][0] #hardcode!!!
         # тут какие-то питоновские фишки можно попробовать
-        # считаем u и v
-        for i in range(len(C) - 1):  # строки
-            for j in range(len(C[0])):  # столбцы
-                if x1[i][j] != 0:
-                    v[j] = C[i][j] + u[i]
-                    u[i + 1] = -C[i + 1][j] + v[j]
+        # считаем u и v тут неверно, надо слау составить
+        #        for i in range(len(C) - 1):  # строки
+        #            for j in range(len(C[0])):  # столбцы
+        #                if x1[i][j] != 0:
+        #                    v[j] = C[i][j] + u[i]
+        #                    u[i + 1] = -C[i + 1][j] + v[j]
 
-        for j in range(len(C[0])):
-            if x1[len(u) - 1][j] != 0:
-                v[j] = C[len(u) - 1][j] + u[len(u) - 1]
+        #        for j in range(len(C[0])):
+        #            if x1[len(u) - 1][j] != 0:
+        #                v[j] = C[len(u) - 1][j] + u[len(u) - 1]
 
-        print("u,v : ", u, v)
-
+        matrix = np.zeros((len(array), len(x1)+len(x1[0])))
+        b = np.zeros(len(array))
+        for i in range(len(array)):
+            matrix[i][array[i][0]] = -1
+            matrix[i][len(x1)+array[i][1]] = 1
+            b[i] = C[array[i][0]][array[i][1]]
+        #print("u,v : ", u, v)
+        vec = np.linalg.solve(matrix, b)
+        u = vec[0:len(x1)-1]
+        v = vec[len(x1):len(x1)+len(x1[0])-1]
         # проверяем оптимальность в пустых клетках типа перебором или по хитрому
         # Beda if v_j - u_i > cij
 
@@ -90,23 +104,27 @@ def Alg(x1):
         # цикл пересчёта
 
         array.append([i, j])
-        Modi(True, x1, i, j, 0) #Находит цикл пересчёта, записывает его в array
+        Modi(True, x1, i, j, 0)  # Находит цикл пересчёта, записывает его в array
         print("цикл пересчёта: ", array)
-        #там перый и последный элементы повторяются
+        # там перый и последный элементы повторяются
         array.remove(array[0])
-        #print(array)
+        # print(array)
 
         # найди минимум из "-" клеток
         mina = findMin(array, x1)
         print("min- = ", mina)
         # пересчёт объёмов груза:
-        for i in range(0, len(array), 2):  # "-"
+        i = 0
+        while i < len(array):  # "-"
             x1[array[i][0]][array[i][1]] -= mina
+            i += 2
 
-        for i in range(1, len(array), 2):  # "+"
+        i = 1
+        while i < len(array):  # "+"
             x1[array[i][0]][array[i][1]] += mina
+            i += 2
 
-        print("new x1:",x1)
+        print("new x1:", x1)
         array.clear()
         # снова проверяем оптимальность и пересчёт плана
 
@@ -121,7 +139,7 @@ a = [16, 13, 8, 7, 9]
 
 b = [8, 20, 6, 9]
 
-#первый опорный вектор, полученный методом С-З угла
+# первый опорный вектор, полученный методом С-З угла
 x1 = [
     [8, 0, 0, 0, 0],
     [8, 12, 0, 0, 0],
