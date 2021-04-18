cc05031aed8c5ea363986429b0655b5d4140dc5d
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2021-04-18 09:02:41 +0300

    Update Grad.py

--- Grad.py
+++ Grad.py
@@ -1,146 +1,138 @@
-import math as m
 import numpy as np
+from scipy.optimize import minimize
 
 
-def F(x1, x2):
-    return x1 ** 2 + x2 ** 2 + m.sin(2 * x1 + 3 * x2) + 3 * x1 + 2 * x2
+def f(x):
+    #return (x[0] ** 2 +  x[1] ** 2 + np.sin(2 * x[0] + 3 * x[1]) + 3 * x[0] + 2 * x[1])
+    return max((x[0]+1) ** 2 + 5 * x[1] ** 2, (x[0]-1) ** 2 +5*x[1] ** 2)
+def gradf(x):
+    f1 = (x[0]+1) ** 2 + 5 * x[1] ** 2
+    f2 = (x[0]-1) ** 2 + 5 * x[1] ** 2
+    if f1>f2:
+        return np.array([2 * (x[0] + 1), 10 * x[1]])
+    else:
+        return np.array([2 * (x[0] - 1), 10 * x[1]])
+    #return np.array([3 + 2 * x[0] + 2 * np.cos(2 * x[0] + 3 * x[1]),
+    #                  2 + 2 * x[1] + 3 * np.cos(2 * x[0] + 3 * x[1])])
+
+def H(x):
+    return
+    #return np.array([[2 - 4 * np.sin(2 * x[0] + 3 * x[1]), -6 * np.sin(2 * x[0] + 3 * x[1])],
+    #                [-6 * np.sin(2 * x[0] + 3 * x[1]), 2 - 9 * np.sin(2 * x[0] + 3 * x[1])]])
 
 
-def GradFx(x1, x2):
-    return 2 * x1 + 2 * m.cos(2 * x1 + 3 * x2) + 3
+def goldenSearch(f, a, b, eps):
+    al2 = (3 - 5 ** (1 / 2)) / 2
+    count = 0
 
+    (a_k, b_k) = (min(a, b), max(a, b))
+    length = b_k - a_k
+    if length <= eps:
+        return a_k, b_k
 
-def GradFy(x1, x2):
-    return 2 * x2 + 3 * m.cos(2 * x1 + 3 * x2) + 2
+    lambda_k = a_k + al2 * length
+    mu_k = b_k - al2 * length
+    f_lambda_k = f(lambda_k)
+    f_mu_k = f(mu_k)
+    count += 2
 
-
-def H11(x1, x2):
-    return 2 - 4 * m.sin(2 * x1 + 3 * x2)
-
-
-def H12(x1, x2):
-    return -6 * m.sin(2 * x1 + 3 * x2)
-
-
-def H21(x1, x2):
-    return -6 * m.sin(2 * x1 + 3 * x2)
-
-
-def H22(x1, x2):
-    return 2 - 9 * m.sin(2 * x1 + 3 * x2)
-
-def GoldenSelectionSearch(a,b,x1,x2,pk,eps):# а > 0
-    a_new = a
-    b_new = b
-    i = 1
-    alf = (3 - m.sqrt(5)) / 2
-    lam_all = []
-    mu_all = []
-    while (b_new - a_new) > eps:
-        lam_k = a_new + alf * (b_new - a_new)
-        mu_k = a_new + b_new - lam_k
-        # print("Итерация: ", i, "; lambda_k = ", lam_k, "; mu_k = ", mu_k)
-        i += 1
-        if F(x1+lam_k*pk[0], x2+lam_k*pk[1]) < F(x1+mu_k*pk[0], x2+mu_k*pk[1]):
-            b_new = mu_k
+    while length > eps:
+        if f_lambda_k < f_mu_k:
+            b_k = mu_k
+            mu_k = lambda_k
+            f_mu_k = f_lambda_k
+            length = b_k - a_k
+            lambda_k = a_k + al2 * length
+            f_lambda_k = f(lambda_k)
+            count += 1
         else:
-            a_new = lam_k
-        lam_all.append(lam_k)
-        mu_all.append(mu_k)
-    return [a_new, b_new]
+            a_k = lambda_k
+            lambda_k = mu_k
+            f_lambda_k = f_mu_k
+            length = b_k - a_k
+            mu_k = b_k - al2 * length
+            f_mu_k = f(mu_k)
+            count += 1
+    return (a_k + b_k) / 2
 
 
+def StepCrush(f, x, p, grad):
+    alpha = 1
+    lambd = 0.5
+    delta = 0.5
+    while f(x + alpha * p) - f(x) > 1 * alpha * delta * np.dot(grad(x), p):
+        alpha *= lambd
+    return x + alpha * p
 
-def Grad1(eps, x_0, y_0, alpha):
-    f = False
-    x_old = x_0
-    y_old = y_0
-    x_new = x_0
-    y_new = y_0
+
+def SearchAlpha(f, x, p, grad=0):
+    delt = 0.5
+    b = delt
+    a = 0
+    while f(x + a * p) < f(x + b * p):
+        a += delt
+        b += delt
+
+    alpha = goldenSearch(lambda y: f(x + y * p), a, b, eps)
+    return x + alpha * p
+
+
+def SecondGrad(f, grad, hesse, x0, eps, alpha):
+    normGrad = 3
+    x = x0
+    vec = []
+    vec.append(x)
     count = 0
-    while (not f) or (np.linalg.norm([GradFx(x_new, y_new), GradFy(x_new, y_new)]) > eps):
-        f = True
-        tmp1 = x_new
-        tmp2 = y_new
-        x_new = x_old - alpha * GradFx(x_old, y_old)
-        y_new = y_old - alpha * GradFy(x_old, y_old)
-        x_old = tmp1
-        y_old = tmp2
+    while normGrad >= eps:
         count += 1
-        print(x_new, y_new)
-        print(np.linalg.norm([GradFx(x_new, y_new), GradFy(x_new, y_new)]))
-    return count
+        H = hesse(x)
+        b = -1 * grad(x)
+        p = np.linalg.solve(H, b)
+        x = alpha(f, x, p, grad)
+        normGrad = np.linalg.norm(grad(x), ord=2)
+        vec.append(x)
+    print("итерации", count)
+    print("x_k-1 = ", vec[len(vec)-2])
+    x = np.round(np.array(x),4)
+    return x
 
+def FirstGrad(f, grad, x0, eps, alpha):
 
-# тут не понятно что такое p_k и что за матрицу вообще брать надо
-def Grad2Prop(eps, x_0, y_0, lam, delt):  # 0<lam<1, 0<delt<0.5
-    f = False
-
-    x_old = x_0
-    y_old = y_0
-    x_new = x_0
-    y_new = y_0
+    normGrad = 3
+    x = x0
     count = 0
-    while (not f) or (np.linalg.norm([GradFx(x_new, y_new), GradFy(x_new, y_new)]) > eps):
-        f = True
+    vec = []
+    vec.append(x)
+    while normGrad >= eps:
         count += 1
-        alpha = 1
-        A = [[H11(x_old, y_old), H12(x_old, y_old)], [H21(x_old, y_old), H22(x_old, y_old)]]
-        b = [-GradFx(x_old, x_old), -GradFy(x_old, x_old)]
-        pk = np.linalg.solve(A, b)
+        x = x - alpha * grad(x)
+        print(x)
+        #H_x = H(x)
+        #w, v = np.linalg.eigh(H_x)
+        #print("Собственные числа", w)
+        normGrad = np.linalg.norm(grad(x), ord=2)
+        vec.append(x)
+    print("итерации", count)
+    print("x_k-1 = ", vec[len(vec) - 2])
+    x = np.round(np.array(x), 4)
+    return x
 
-        while F(x_old + alpha * pk[0], y_old + alpha * pk[1]) - F(x_old, y_old) > alpha * delt * (
-                GradFx(x_old, y_old) * pk[0] + GradFy(x_old, y_old) * pk[1]):
-            alpha = alpha * lam
-
-        tmp1 = x_new
-        tmp2 = y_new
-        x_new = x_old + alpha * pk[0]
-        y_new = y_old + alpha * pk[1]
-        x_old = tmp1
-        y_old = tmp2
-    return count
-
-def Grad2desc(eps, x_0, y_0):  # 0<lam<1, 0<delt<0.5
-    f = False
-
-    x_old = x_0
-    y_old = y_0
-    x_new = x_0
-    y_new = y_0
-    count = 0
-    while (not f) or (np.linalg.norm([GradFx(x_new, y_new), GradFy(x_new, y_new)]) > eps):
-        f = True
-        count += 1
-        alpha = 1
-        A = [[H11(x_old, y_old), H12(x_old, y_old)], [H21(x_old, y_old), H22(x_old, y_old)]]
-        b = [-GradFx(x_old, x_old), -GradFy(x_old, x_old)]
-        pk = np.linalg.solve(A, b)
-
-        #одномерная минимизация по а# перед этим проверить значения на концах отрезка
-        #т.е. сначала дроблением а потом ЗС
-        #не понятно как выбирать отрезок
-        # А потом методом ЗС, функцию чтобы меньше вычислять
-        a = GoldenSelectionSearch(0, 5, x_old, y_old, pk, 0.001)  # а > 0
-        alpha = (a[0] + a[1]) / 2
-
-        tmp1 = x_new
-        tmp2 = y_new
-        x_new = x_old + alpha * pk[0]
-        y_new = y_old + alpha * pk[1]
-        x_old = tmp1
-        y_old = tmp2
-    return count
+if __name__ == "__main__":
+    x0 = [0, 0]
+    eps = 0.1
+    print(FirstGrad(f, gradf, x0, eps, 0.5))
 
 
-# начальне приближение# выбирала на глазок
-x_0 = -2.5  # -3
-y_0 = -2.5  # -2
-# шаг
-lam = 0.1  # 0.001 0.01
-# точность относительно минимизируюшей функции
-eps = 0.01
-print("итерации", Grad1(0.1, -3, -2, 0.101))
-# print(Grad2Prop(eps, x_0, y_0, 0.1, 0.1))
 
-
+    '''x0 = [-1, 0]
+    eps = 0.1
+    sol = minimize(f, x0, method='Nelder-Mead', tol=10 ** -10).x
+    while eps > 10 ** -4:
+        print(eps)
+        #print("2GradAlpha",round(np.linalg.norm(SecondGrad(f, gradf, H, x0, eps, SearchAlpha)-sol),6))
+        #print("2GradAlpha-Sol", SecondGrad(f, gradf, H, x0, eps, SearchAlpha))
+        #print("2GradDr",round(np.linalg.norm(SecondGrad(f, gradf, H, x0, eps, StepCrush) - sol),6))
+        #print("2GradDr-Sol",SecondGrad(f, gradf, H, x0, eps, StepCrush))
+        print("OneStep",round(np.linalg.norm(FirstGrad(f, gradf, x0, eps, 0.126) - sol),6))
+        print("OneStep-Sol",FirstGrad(f, gradf, x0, eps, 0.126) )
+        eps = eps / 10'''
